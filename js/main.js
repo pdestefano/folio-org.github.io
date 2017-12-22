0efb3512fd0cf38e648fe1d1d8d99381f2c07190
pdestefano <pdestefano@ebsco.com>
2017-12-22 16:25:05 -0500

    Clean up and simplified javascript a bit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,10 @@
-//= require ../lib/_jquery
-//= require ../lib/_jquery_ui
-//= require ../lib/_jquery.tocify
-//= require ../lib/_imagesloaded.min
-(function (global) {
-  'use strict';
+$(function() {
 
- // var closeToc = function() {
-//    $(".tocify-wrapper").removeClass('open');
- //   $("#nav-button").removeClass('open');
- // };
+    // $('.collapse').collapse('hide');
+    $('.list-group-item.active').parent().parent('.collapse').collapse('show');
 
-/*
-  var makeToc = function() {
-    global.toc = $("#toc").tocify({
-      selectors: 'h1, h2, h3',
-      extendPage: false,
-      scrollTo: 50,
-      showAndHide: false
-    }).data('toc-tocify');
-*/
-
-
-  var makeToc = function() {
-    global.toc = $("#toc").tocify({
+ 
+    var toc = $("#toc").tocify({
       selectors: 'h1, h2',
       extendPage: false,
       theme: 'none',
@@ -38,59 +20,31 @@
       }
     }).data('toc-tocify');
 	
-    $("#nav-button").click(function() {
-      $(".tocify-wrapper").toggleClass('open');
-      $("#nav-button").toggleClass('open');
-      return false;
-    });
-
-    $(".page-wrapper").click(closeToc);
-    $(".tocify-item").click(closeToc);
-  };
-
-  // Hack to make already open sections to start opened,
-  // instead of displaying an ugly animation
-  function animate() {
-    setTimeout(function() {
-      toc.setOption('showEffectSpeed', 180);
-    }, 50);
-  }
-
-  $(function() {
-	    $('.list-group-item.active').parent().parent('.collapse').collapse('show'); 
-    makeToc();
-    animate();
-   setupLanguages($('body').data('languages'));
-    $('.content').imagesLoaded( function() {
-      global.toc.calculateHeights();
-    });
-	
-	
-    var pages = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+ //   var pages = new Bloodhound({
+ //       datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
         // datumTokenizer: Bloodhound.tokenizers.whitespace,
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
+ //       queryTokenizer: Bloodhound.tokenizers.whitespace,
 
-        prefetch: baseurl + '/search.json'
-    });	
-   $('#search-box').typeahead({
-        minLength: 0,
-        highlight: true
-    }, {
-        name: 'pages',
-        display: 'title',
-        source: pages
-    });
+ //       prefetch: baseurl + '/search.json'
+ //   });
 
-    $('#search-box').bind('typeahead:select', function(ev, suggestion) {
-        window.location.href = suggestion.url;
-    });
-	
+//    $('#search-box').typeahead({
+//        minLength: 0,
+//        highlight: true
+//    }, {
+//        name: 'pages',
+//        display: 'title',
+//        source: pages
+//    });
+
+//    $('#search-box').bind('typeahead:select', function(ev, suggestion) {
+//        window.location.href = suggestion.url;
+//    });
+
+
     // Markdown plain out to bootstrap style
     $('#markdown-content-container table').addClass('table');
-    $('#markdown-content-container img').addClass('img-responsive');	
-	
-	
-	
-  });
-})(window);
+    $('#markdown-content-container img').addClass('img-responsive');
+
+
+});
